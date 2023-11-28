db4b71c7435360cbe56b8c2f95c617cad09b8bcf
Vijay Rajput <vijayrajputhp@gmail.com>
2023-11-28 19:52:34 +0530

    added react components --commented

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -3,6 +3,22 @@ import MobileMenu from '../scripts/modules/MobileMenu'
 import RevealOnScroll from './modules/RevealOnScroll'
 import StickyHeader from './modules/StickyHeader'
 
+
+//React Related Code Goes Here
+// import React from 'react'
+// import  ReactDOM from 'react-dom/client'
+
+// Import React Components That We Created
+// import MyAmazingComponent from './modules/MyAmazingComponent'
+
+
+
+// ReactDOM.createRoot(document.querySelector("#my-react-example")).render(
+//     <React.StrictMode>
+//         <MyAmazingComponent/>
+//     </React.StrictMode>
+// )
+
 new StickyHeader();
 new RevealOnScroll(document.querySelectorAll(".feature-item"), 75)
 new RevealOnScroll(document.querySelectorAll(".testimonial"), 60)
--- webpack.config.js
+++ webpack.config.js
@@ -57,7 +57,17 @@ let config = {
     plugins: pages,
     module: {
         rules: [
-            cssConfig
+            cssConfig,
+            {
+                test: /\.js$/,
+                exclude: /(node_modules)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-react','@babel/preset-env']
+                    }
+                }
+            }
         ]
     }
 }
@@ -81,16 +91,6 @@ if (currentTask == 'dev') {
 }
 
 if (currentTask == 'build') {
-    config.module.rules.push({
-        test: /\.js$/,
-        exclude: /(node_modules)/,
-        use: {
-            loader: 'babel-loader',
-            options: {
-                presets: ['@babel/preset-env']
-            }
-        }
-    })
 
     cssConfig.use.unshift(MiniCssExtractPlugin.loader)
     config.output = {
